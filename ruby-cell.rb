b3ae0f2c548b38e8b42ae57518c878ce1ac60c8f
Cody Stringham <cs.negativ@gmail.com>
2014-01-22 14:23:15 -0700

    Fixes grid printing

--- ruby-cell.rb
+++ ruby-cell.rb
@@ -53,12 +53,27 @@ def check_cells_for_future_state
     end
   end
 
+print_cell_grid_and_counter
+end
 
+
+def print_cell_grid_and_counter
+  
+  system"clear"
+  @cell_grid.each do |row|
+    row.each do |cell|
+      print cell
+      print " "
+    end
+    print "\n"  
+  end
+
+  @tick_count += 1
+  print "Days passed: #{@tick_count}"
 end
 
 
 def end_game_print_result
-  @tick_count += 1
   print "#{@tick_count} ticks were played, end of game."
 end
 
