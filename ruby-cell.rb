05251cc6c7520d0d3adbb5138ab39a7b095e7776
Cody Stringham <cs.negativ@gmail.com>
2014-01-26 13:28:45 -0700

    Rough finish on game

--- ruby-cell.rb
+++ ruby-cell.rb
@@ -1,37 +1,23 @@
-#Make the game end when all cells are dead.
-#Look at each cell one at a time to see if its alive or dead.
-#Check to see how many cells are alive around that cell.
-#Kill or give life to a cell if one of the rules are true.
-#Print out the grid of cells after each tick.
-
-#Live cell with < 2 neighboring cells = dead
-#Live cell with > 3 neightboring cells = dead
-#dead cell with 3 neighboring cells = live
-#Live cells with 2-3 neighboring cells = live 
-
 class CellGame
 
 def initialize
 
-  puts "How big do you want this game?"
-  @size = gets.chomp.to_i
+  puts "How tall do you want the grid?"
+  @tall = gets.chomp.to_i
+  puts "How wide do you want the grid?"
+  @wide = gets.chomp.to_i
+  @days_passed = 0
+  @cell_grid = Array.new(@tall) { Array.new(@wide) }
+  @grid_storage = Array.new(@tall) { Array.new(@wide) }
 
-  #Creates display and dump grids
-  @cell_grid = Array.new(@size, "") { Array.new(@size, "") }
-  @cell_grid_dump = Array.new(@size, "") { Array.new(@size, "") }
-
-  #Counts how many ticks the game has played
-  @tick_count = 0
-
-  #Goes to next method
   fill_grid_with_random_cells
 end
 
 def fill_grid_with_random_cells
   @cell_grid.each do |row|
     row.map! do |cell|
-      roll = rand(100)
-        if roll > 51
+      roll = rand(20)
+        if roll == 2
           "•"
         else
           " "
@@ -43,17 +29,23 @@ def fill_grid_with_random_cells
 end
 
 def check_for_any_alive_cells
+  contains_alive = 0
 
-  # Each loop that checks each cell for alive cells
   @cell_grid.each do |row|
     row.each do |cell|
       if cell.include?("•")
-        check_cells_for_future_state
-      else
-        end_game_print_result
+        contains_alive += 1
       end
     end
   end
+
+  if contains_alive >= 1
+    check_cells_for_future_state
+  else
+    print "No alive cells found"
+    end_game_print_result
+  end
+
 end
 
 
@@ -61,76 +53,103 @@ def check_cells_for_future_state
 
   @cell_grid.each_with_index do |row, row_index|
     row.each_with_index do |cell, cell_index|
-      @live_neighbors = 0
-
+      
       add_row_shift = (row_index + 1)
-      if add_row_shift == @size
-        add_row_shift = 0
-      end
+        if add_row_shift == @tall || add_row_shift == @wide
+          add_row_shift = 0
+        end
 
       add_cell_shift = (cell_index + 1)
-      if add_cell_shift == @size
-        add_cell_shift = 0
-      end
-
-      def does_this_include_alive(cell)
-        if cell.include?("•")
-          @live_neighbors +=1
+        if add_cell_shift == @tall || add_cell_shift == @wide
+          add_cell_shift = 0
         end
-      end
+
+
+      live_neighbors = 0
+
+      #def does_this_include_alive(cell)
+       # if cell.include?("•")
+           #live_neighbors += 1 
+        #end
+      #end
       
       top_left_cell = @cell_grid[(row_index - 1)][(cell_index - 1)] 
-        does_this_include_alive(top_left_cell)
+        #does_this_include_alive(top_left_cell)
+        if top_left_cell.include?("•")
+          live_neighbors += 1
+        end
 
       top_cell = @cell_grid[(row_index - 1)][(cell_index)]
-        does_this_include_alive(top_cell)
-
+        #does_this_include_alive(top_cell)
+        if top_cell.include?("•")
+          live_neighbors += 1
+        end
 
       top_right_cell = @cell_grid[(row_index - 1)][(add_cell_shift)]
-         does_this_include_alive(top_right_cell)
-
+         #does_this_include_alive(top_right_cell)
+         if top_right_cell.include?("•")
+          live_neighbors += 1
+        end
 
       right_cell = @cell_grid[(row_index)][(add_cell_shift)]
-        does_this_include_alive(right_cell)
-
+        #does_this_include_alive(right_cell)
+        if right_cell.include?("•")
+          live_neighbors += 1
+        end
 
       bottom_right_cell = @cell_grid[(add_row_shift)][(add_cell_shift)]
-        does_this_include_alive(bottom_right_cell)
-
+        #does_this_include_alive(bottom_right_cell)
+        if bottom_right_cell.include?("•")
+          live_neighbors += 1
+        end
 
       bottom_cell = @cell_grid[(add_row_shift)][(cell_index)]
-        does_this_include_alive(bottom_cell)
-
+        #does_this_include_alive(bottom_cell)
+        if bottom_cell.include?("•")
+          live_neighbors += 1
+        end
 
       bottom_left_cell = @cell_grid[(add_row_shift)][(cell_index - 1)] 
-        does_this_include_alive(bottom_left_cell)
-
+        #does_this_include_alive(bottom_left_cell)
+        if bottom_left_cell.include?("•")
+          live_neighbors += 1
+        end
 
       left_cell = @cell_grid[(row_index)][(cell_index - 1)] 
-        does_this_include_alive(left_cell)
+        #does_this_include_alive(left_cell)
+        if left_cell.include?("•")
+          live_neighbors += 1
+        end
 
 
-      print "[#{row_index}], [#{cell_index}]: #{@live_neighbors} live neighbors"
-      print "\n"
 
-      if @live_neighbors == 2
-        #alive cell
-      elsif @live_neighbors == 3
-        #alive cell
+      if live_neighbors == 2 || live_neighbors == 3
+        @grid_storage[(row_index)][(cell_index)] = "•"
+
       else
-        #kill cell
+        @grid_storage[(row_index)][(cell_index)] = " "
       end
 
     end
   end
+  
+  update_cell_grid
+end
+
+def update_cell_grid
+  @cell_grid = @grid_storage.clone
+  @grid_storage.each_with_index do |row, x|
+      @cell_grid[x] = row.clone
+    end
+
+  @days_passed += 1
 
   print_cell_grid_and_counter
 end
 
 
-
 def print_cell_grid_and_counter
-  
+
   system"clear"
   @cell_grid.each do |row|
     row.each do |cell|
@@ -139,16 +158,15 @@ def print_cell_grid_and_counter
     print "\n"  
   end
 
-  @tick_count += 1
   print "\n"
-  print "Days passed: #{@tick_count}"
-  sleep(0.5)
+  print "Days passed: #{@days_passed}"
+  sleep(0.15)
   check_for_any_alive_cells
 end
 
 
 def end_game_print_result
-  print "#{@tick_count} ticks were played, end of game."
+  print "#{@days_passed} days have passed, end of game."
 end
 
 
